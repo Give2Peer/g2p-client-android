5a79a1a16846ecb74aadb1ce7983bc3946573905
Goutte <antoine.goutenoir@gmail.com>
2015-09-23 08:04:13 +0200

    Add Profile basic UI skeleton.

---
+++ app/src/main/java/org/give2peer/give2peer/activity/ProfileActivity.java
@@ -0,0 +1,40 @@
+package org.give2peer.give2peer.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+
+import org.give2peer.give2peer.Application;
+import org.give2peer.give2peer.R;
+
+
+/**
+ * The profile activity.
+ * It requires the user to be registered.
+ * - User informations
+ * - Items added by the user
+ */
+public class ProfileActivity extends ActionBarActivity
+{
+    Application app;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        app = (Application) getApplication();
+
+        Log.d("G2P", "Starting profile activity.");
+
+
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        // If the user is not registered, let's forward him to the registration activity.
+        app.requireRegistration(this);
+    }
+}
