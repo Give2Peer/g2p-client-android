7fe5aef99f336f9500eb38e8e26283ea026356b3
Goutte <antoine.goutenoir@gmail.com>
2016-12-11 18:39:48 +0100

    Clean up `FindItemsResponse`. ⥺

--- app/src/main/java/org/give2peer/karma/response/FindItemsResponse.java
+++ app/src/main/java/org/give2peer/karma/response/FindItemsResponse.java
@@ -7,18 +7,15 @@ import java.util.Collection;
 import java.util.List;
 
 /**
- * GSON recipient for the response of `POST` `/item` request.
+ * GSON recipient for the response of `GET` `/items/around/{lat}/{lon}` request.
  */
 public class FindItemsResponse
 {
-    int karma = 0;
+    int    karma = 0;
     Item[] items;
 
-    public FindItemsResponse() {} // maybe needed, maybe not
-
-    /** @return the item that was added, as it is now in the server's database.                   *//****/
-    public Item[] getItems() { return items;                                                       }
-    /** @return the amount of karma gained by the action of finding these items                   *//**/
-    public int getKarma()    { return karma;                                                       }
-
+    /** @return the item that was added, as it is now in the server's database. */
+    public Item[] getItems() { return items; }
+    /** @return the amount of karma gained by the action of finding these items. */
+    public int getKarma()    { return karma; }
 }
